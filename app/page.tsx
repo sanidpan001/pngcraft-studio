c94a8fb08bdcddc7414ffeedafef3e53c894cec7
sanidpan001 <sandipanghosh227@gmail.com>
2026-09-11 15:28:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       {/* Ad Slot 1 - Top banner */}
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4">
-        <AdSlot width={728} height={90} className="mx-auto" />
+        <AdSlot />
       </div>
 
       <main>
